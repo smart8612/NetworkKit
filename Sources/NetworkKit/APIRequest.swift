cdeccca9d135cc9ae22c883738a869d4f6cbddfd
JeongTaek Han <smart8612@gmail.com>
2023-06-14 15:42:50 +0900

    doc: documented APIRequest protocol #1
    write abstract document about APIRequest

--- Sources/NetworkKit/APIRequest.swift
+++ Sources/NetworkKit/APIRequest.swift
@@ -8,13 +8,24 @@
 import Foundation
 
 
+/// A type that declares an api request form that a network controller can perform.
 public protocol APIRequest {
     
-    associatedtype Response
+    /// A type that represent a response form of api request.
+    associatedtype Response: Codable
     
+    /// A property that contains api request information.
     var urlRequest: URLRequest? { get }
+    
+    /// Decode response data to response form.
+    /// - Parameter data: The data that represent remote server's response.
+    /// - Returns: An instance decoded to ``Response`` type.
     func decodeResponse(data: Data) throws -> Response
     
+    
+    /// Verify response whether an exception occurred.
+    /// - Parameter response: The response information that represent remote server's returned.
+    /// - Returns: A boolean value that represent whether an exception has been occurred or not.
     @discardableResult
     func verify(response: URLResponse) throws -> Bool
     
