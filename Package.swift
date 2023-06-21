72321d1689c6182db0696543c6818c2eb1adbf6d
JeongTaek Han <smart8612@gmail.com>
2023-06-21 16:38:46 +0900

    refactor: delete swiftlint dependency #4

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/realm/SwiftLint", from: "0.52.2"),
          .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
@@ -24,8 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NetworkKit",
-            dependencies: [],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
+            dependencies: []),
         .testTarget(
             name: "NetworkKitTests",
             dependencies: ["NetworkKit"]),
